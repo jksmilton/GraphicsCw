7625305fc9ea5a56ccf50dd163a7095ab7e4774e
Jordan Milton <jksmilton@gmail.com>
2012-11-14 01:09:38 +0000

    draws paths but doesnt animate correctly

--- GraphicsCw/graphics.cpp
+++ GraphicsCw/graphics.cpp
@@ -98,9 +98,9 @@ private:
 	GLuint vertexshader, fragmentshader; //reference shaders
 	GLuint shaderprogram;
 	vector<vertex> sphereCoordinates;
-	GLuint MatrixID;
+	GLuint MatrixID, TranslationID;
 	float oldTime;
-	glm::mat4 MVP, Model, Projection, View;
+	glm::mat4 MVP, Model, Projection, View, Translation;
 
 	vector<vertex> getSphereCoordinates(float radius, int meridians, int parallels) {
 	
@@ -165,13 +165,13 @@ public:
 		glVertexAttribPointer((GLuint) 0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 		glBindAttribLocation(shaderprogram, 0, "in_position");
 		//main loop
-		Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+		Projection = glm::perspective(45.0f, 1.f, 0.1f, 100.0f);
 		View       = glm::lookAt(
 			glm::vec3(2,2,2), // Camera is at (4,3,3), in World Space
 			glm::vec3(0,0,0), // and looks at the origin
 			glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
 		);
-
+		Translation = glm::mat4(1.f);
 		// Model matrix : an identity matrix (model will be at the origin)
 		Model = glm::mat4(1.0f);  // Changes for each model !
 		// Our ModelViewProjection : multiplication of our 3 matrices
@@ -179,11 +179,8 @@ public:
 		// Get a handle for our "MVP" uniform.
 		// Only at initialisation time.
 		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
- 
-		// Send our transformation to the currently bound shader,
-		// in the "MVP" uniform
-		// For each model you render, since the MVP will be different (at least the M part)
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		TranslationID = glGetUniformLocation(shaderprogram, "translation");
+	
 
 
 		return true;
@@ -192,33 +189,32 @@ public:
 	void render(){
 		glUseProgram(shaderprogram);
 		float rotation, currentTime;
-						
+		glm::mat4 translation;			
 		currentTime = glfwGetTime();
 		rotation = (currentTime - oldTime) * speed;
 		oldTime = currentTime;
 		
-		printf("rotation: %f\n", rotation);
+		//printf("rotation: %f\n", rotation);
 
 		glBindVertexArray(vao[0]);
 		//check("bind array");
 		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 		//check("bind buffer");
 		
-		View = glm::rotate(View, rotation, glm::vec3(1, 1, 1));
-
+		//View = glm::rotate(View, rotation, glm::vec3(1, 1, 1));
+		Translation = glm::rotate(Translation, rotation, glm::vec3(1,1,0));
 		// Model matrix : an identity matrix (model will be at the origin)
 		Model = glm::mat4(1.0f);  // Changes for each model !
 		// Our ModelViewProjection : multiplication of our 3 matrices
 		MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 		// Get a handle for our "MVP" uniform.
-		// Only at initialisation time.
-		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
+
  
 		// Send our transformation to the currently bound shader,
 		// in the "MVP" uniform
 		// For each model you render, since the MVP will be different (at least the M part)
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
+		glUniformMatrix4fv(TranslationID, 1, GL_FALSE, &Translation[0][0]);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawArrays(GL_TRIANGLES, 0, sphereCoordinates.size());
 
@@ -231,11 +227,11 @@ class coneRotate {
 	GLuint vao[1], vbo[1];
 	GLchar *vertexsource, *fragmentsource; //receive contents of shader source
 	GLuint vertexshader, fragmentshader; //reference shaders
-	GLuint shaderprogram;
+	GLuint shaderprogram, TranslationID;
 	vector<vertex> coneCoordinates;
 	GLuint MatrixID;
 	float oldTime;
-	glm::mat4 MVP, Model, Projection, View;
+	glm::mat4 MVP, Model, Projection, View, Translate;
 	vector<vertex> getConeCoordinates(float height, float radius, int points){
 
 		float edgeShift = 2 * PI / points;
@@ -291,13 +287,13 @@ class coneRotate {
 		glVertexAttribPointer((GLuint) 0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 		glBindAttribLocation(shaderprogram, 0, "in_position");
 		//main loop
-		Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+		Projection = glm::perspective(45.0f, 1.f, 0.1f, 100.0f);
 		View       = glm::lookAt(
 			glm::vec3(4,4,3), // 
 			glm::vec3(0,0,0), // and looks at the origin
 			glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
 		);
-
+		Translate = glm::mat4(1.f);
 		// Model matrix : an identity matrix (model will be at the origin)
 		Model = glm::mat4(1.0f);  // Changes for each model !
 		// Our ModelViewProjection : multiplication of our 3 matrices
@@ -305,12 +301,7 @@ class coneRotate {
 		// Get a handle for our "MVP" uniform.
 		// Only at initialisation time.
 		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
- 
-		// Send our transformation to the currently bound shader,
-		// in the "MVP" uniform
-		// For each model you render, since the MVP will be different (at least the M part)
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
+		TranslationID = glGetUniformLocation(shaderprogram, "translation");
 
 		return true;
 	}
@@ -323,7 +314,7 @@ class coneRotate {
 		rotation = (currentTime - oldTime) * speed;
 		oldTime = currentTime;
 		
-		printf("rotation: %f\n", rotation);
+		//printf("rotation: %f\n", rotation);
 
 		glBindVertexArray(vao[0]);
 		//check("bind array");
@@ -337,14 +328,12 @@ class coneRotate {
 		// Our ModelViewProjection : multiplication of our 3 matrices
 		MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 		// Get a handle for our "MVP" uniform.
-		// Only at initialisation time.
-		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
  
 		// Send our transformation to the currently bound shader,
 		// in the "MVP" uniform
 		// For each model you render, since the MVP will be different (at least the M part)
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
+		glUniformMatrix4fv(TranslationID,1 , GL_FALSE, &Translate[0][0]);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawArrays(GL_TRIANGLES, 0, coneCoordinates.size());
 
@@ -357,12 +346,12 @@ class SphereNormals {
 	GLuint vao[2], vbo[2];
 	GLchar *vertexsource, *fragmentsource; //receive contents of shader source
 	GLuint vertexshader, fragmentshader; //reference shaders
-	GLuint shaderprogram;
+	GLuint shaderprogram, TranslationID;
 	vector<vertex> sphereCoordinates;
 	vector<vertex> sphereNormals;
 	GLuint MatrixID;
 	float oldTime;
-	glm::mat4 MVP, Model, Projection, View;
+	glm::mat4 MVP, Model, Projection, View, Translate;
 	vector<vertex> getSphereCoordinates(float radius, int meridians, int parallels) {
 	
 		vector<vertex> returnedPositions;
@@ -410,13 +399,15 @@ class SphereNormals {
 				sphereNormals.push_back(currentvert);
 				sphereNormals.push_back(normalVert);
 
+				//printf("current vert: %f,%f,%f; normal vert: %f,%f,%f\n", currentvert.x,currentvert.y,currentvert.z, normalVert.x,normalVert.y,normalVert.z);
+
 			}
 
-			return returnedPositions;
-
 	}
-};
+	
+	return returnedPositions;
 
+}
 	public:
 
 	bool init(){
@@ -436,13 +427,15 @@ class SphereNormals {
 
 		glBindVertexArray(vao[1]);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-
+		glEnableVertexAttribArray(0);
 		glBufferData(GL_ARRAY_BUFFER, sphereNormals.size() * sizeof(vertex), &(sphereNormals[0]), GL_STATIC_DRAW);
 		glVertexAttribPointer((GLuint) 0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
 		glBindAttribLocation(shaderprogram, 0, "in_position");
-		//main loop
-		Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+		
+		Translate = glm::mat4(1.f);
+
+		Projection = glm::perspective(45.0f, 1.f, 0.1f, 100.0f);
 		View       = glm::lookAt(
 			glm::vec3(2,2,2), // Camera is at (4,3,3), in World Space
 			glm::vec3(0,0,0), // and looks at the origin
@@ -456,7 +449,7 @@ class SphereNormals {
 		// Get a handle for our "MVP" uniform.
 		// Only at initialisation time.
 		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
- 
+		TranslationID = glGetUniformLocation(shaderprogram, "translation");
 		// Send our transformation to the currently bound shader,
 		// in the "MVP" uniform
 		// For each model you render, since the MVP will be different (at least the M part)
@@ -474,7 +467,7 @@ class SphereNormals {
 		rotation = (currentTime - oldTime) * speed;
 		oldTime = currentTime;
 		
-		printf("rotation: %f\n", rotation);
+		//printf("rotation: %f\n", rotation);
 
 		
 		
@@ -486,7 +479,7 @@ class SphereNormals {
 		MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 		// Get a handle for our "MVP" uniform.
 		// Only at initialisation time.
-		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
+		
  
 		// Send our transformation to the currently bound shader,
 		// in the "MVP" uniform
@@ -499,10 +492,10 @@ class SphereNormals {
 		//check("bind buffer");
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		//glDrawArrays(GL_TRIANGLES, 0, sphereCoordinates.size());
-
-		glBindVertexArray(vao[0]);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		glDrawArrays(GL_TRIANGLES, 0, sphereCoordinates.size());
+		glUniformMatrix4fv(TranslationID,1 , GL_FALSE, &Translate[0][0]);
+		glBindVertexArray(vao[1]);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 
 		glDrawArrays(GL_LINES, 0, sphereNormals.size());
 
@@ -510,12 +503,272 @@ class SphereNormals {
 
 };
 
+class Paths { 
+
+	GLuint vao[4], vbo[4];
+	GLchar *vertexsource, *fragmentsource; //receive contents of shader source
+	GLuint vertexshader, fragmentshader; //reference shaders
+	GLuint shaderprogram, TranslationID;
+	vector<vertex> coordinates[4];
+	
+	GLuint MatrixID;
+	float oldTime;
+	glm::mat4 MVP, Model, Projection, View, Translate[4];
+	vector<vertex> getSphereCoordinates(float radius, int meridians, int parallels) {
+	
+		vector<vertex> returnedPositions;
+	
+		float meridianShift, parallelShift;
+		meridianShift = PI/meridians;
+		parallelShift = 2*PI/parallels;
+
+		for(float theta = 0; theta < PI; theta += meridianShift) {
+			//loop through meridians
+		
+			for(float phi = 0; phi < 2*PI; phi += parallelShift) {
+
+			
+				vertex currentvert, nextVertP, nextVertM, nextVertMP;
+	//			printf("%i : ", currentvert);
+				currentvert.x = cosf(theta) * sinf(phi) * radius;
+				nextVertP.x = cosf(theta) * sinf(phi + parallelShift) * radius;
+				nextVertM.x = cosf(theta + meridianShift) * sinf(phi) * radius;
+				nextVertMP.x = cosf(theta + meridianShift) * sinf(phi + parallelShift) * radius;
+		//		printf("%f:", currentvert.x);
+				currentvert.y = sinf(theta) * sinf(phi) * radius;
+				nextVertP.y = sinf(theta) * sinf(phi + parallelShift) * radius;
+				nextVertM.y = sinf(theta + meridianShift) * sinf(phi) * radius;
+				nextVertMP.y = sinf(theta + meridianShift) * sinf(phi + parallelShift) * radius;
+			//	printf("%f:", currentvert.y);
+				currentvert.z = cosf(phi) * radius;
+				nextVertP.z = cosf(phi + parallelShift) * radius;
+				nextVertM.z = currentvert.z;
+				nextVertMP.z = nextVertP.z;
+				//printf("%f\n", currentvert.z);
+			
+				returnedPositions.push_back(currentvert);
+				returnedPositions.push_back(nextVertM);
+				returnedPositions.push_back(nextVertMP);
+				returnedPositions.push_back(currentvert);
+				returnedPositions.push_back(nextVertP);
+				returnedPositions.push_back(nextVertMP);
+				//then through parallels
+
+
+			}
+
+	}
+	
+	return returnedPositions;
+
+}
+	vector<vertex> getConeCoordinates(float height, float radius, int points){
+
+		float edgeShift = 2 * PI / points;
+		vector<vertex> returnedPoints;
+		
+		vertex origin, top;
+
+		origin.x = 0;
+		origin.y = 0;
+		origin.z = 0;
+
+		top.x = 0;
+		top.y = 0;
+		top.z = height;
+
+		for(float theta = 0; theta < 2 * PI; theta+= edgeShift){
+
+			vertex current, next;
+
+			current.x = radius * sinf(theta);
+			next.x = radius * sinf(theta + edgeShift);
+			current.y = radius * cosf(theta);
+			next.y = radius * cosf(theta + edgeShift);
+			current.z = 0;
+			next.z = 0;
+
+			returnedPoints.push_back(current);
+			returnedPoints.push_back(next);
+			returnedPoints.push_back(top);
+			returnedPoints.push_back(current);
+			returnedPoints.push_back(next);
+			returnedPoints.push_back(origin);
+
+		}
+
+		return returnedPoints;
+
+	}
+	
+	void move(float timeDifference, int index){
+		float change = timeDifference * speed / 100;
+		glm::vec4 workingWith = Translate[index][3];
+
+		if(workingWith[0] <  4.f && workingWith[1] >= -4.f){
+
+			workingWith[0] += change;
+			workingWith[1] = -4.f;
+
+		} else if(workingWith[0] <= -4.f && workingWith[1] < 4.f){
+
+			workingWith[1] += change;
+			workingWith[0] = -4.f;
+
+		} else if(workingWith[0] >= 4.f && workingWith[1] > -4.f){
+
+			workingWith[1] =  workingWith[1] - change;
+			workingWith[0] = 4.f;
+
+		} else if(workingWith[0] > - 4.f && workingWith[1] <= -4.f){
+
+			workingWith[0] = workingWith[0] - change;
+			workingWith[1] = 4.f;
+
+		}
+
+		Translate[index] = glm::mat4(
+			glm::vec4(1, 0, 0, 0),
+			glm::vec4(0, 1, 0, 0),
+			glm::vec4(0, 0, 1, 0),
+			workingWith
+			);
+
+	}
+public:
+
+	bool init(){
+
+		shaderprogram = setupShaders(vertexsource, fragmentsource, vertexshader, fragmentshader, "gcw.vert", "gcw.frag");
+		coordinates[0] = getSphereCoordinates(1, 20, 20);
+		coordinates[3] = getSphereCoordinates(1.5, 30, 30);
+		coordinates[2] = getConeCoordinates(1.5, 1, 20);
+		coordinates[1] = getConeCoordinates(2, 0.5, 15);
+
+		glGenVertexArrays(4, vao);
+		glGenBuffers(4, vbo);
+				
+		for(int i = 0; i < 4; i++){
+			glBindVertexArray(vao[i]);
+			glBindBuffer(GL_ARRAY_BUFFER,vbo[i]);
+
+			glEnableVertexAttribArray(0);
+
+			glBufferData(GL_ARRAY_BUFFER, coordinates[i].size() * sizeof(vertex), &(coordinates[i][0]), GL_STATIC_DRAW);
+			glVertexAttribPointer((GLuint) 0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		}
+		
+
+		glBindAttribLocation(shaderprogram, 0, "in_position");
+		
+		for(int i = 0; i< 4;i++)
+			Translate[i] = glm::mat4(1.f);
+
+		Translate[0] = glm::mat4(
+			glm::vec4(1, 0, 0, 0),
+			glm::vec4(0, 1, 0, 0),
+			glm::vec4(0, 0, 1, 0),
+			glm::vec4(4, 4, 0, 1)
+			);
+		Translate[1] = glm::mat4(
+			glm::vec4(1, 0, 0, 0),
+			glm::vec4(0, 1, 0, 0),
+			glm::vec4(0, 0, 1, 0),
+			glm::vec4(-4, 4, 0, 1)
+			);
+		Translate[2] = glm::mat4(
+			glm::vec4(1, 0, 0, 0),
+			glm::vec4(0, 1, 0, 0),
+			glm::vec4(0, 0, 1, 0),
+			glm::vec4(4, -4, 0, 1)
+			);
+		Translate[3] = glm::mat4(
+			glm::vec4(1, 0, 0, 0),
+			glm::vec4(0, 1, 0, 0),
+			glm::vec4(0, 0, 1, 0),
+			glm::vec4(-4, -4, 0, 1)
+			);
+
+		Projection = glm::perspective(45.0f, 1.f, 0.1f, 100.0f);
+		View       = glm::lookAt(
+			glm::vec3(10,10,10), // Camera is at (4,3,3), in World Space
+			glm::vec3(0,0,0), // and looks at the origin
+			glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+		
+		// Model matrix : an identity matrix (model will be at the origin)
+		Model = glm::mat4(1.0f);  // Changes for each model !
+		// Our ModelViewProjection : multiplication of our 3 matrices
+		MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+		// Get a handle for our "MVP" uniform.
+		// Only at initialisation time.
+		MatrixID = glGetUniformLocation(shaderprogram, "MVP");
+		TranslationID = glGetUniformLocation(shaderprogram, "translation");
+		// Send our transformation to the currently bound shader,
+		// in the "MVP" uniform
+		// For each model you render, since the MVP will be different (at least the M part)
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+
+		return true;
+	}
+
+	void render(){
+		glUseProgram(shaderprogram);
+		float timeChange, currentTime;
+						
+		currentTime = glfwGetTime();
+		timeChange = (currentTime - oldTime);
+		oldTime = currentTime;
+		
+		//printf("rotation: %f\n", rotation);
+
+		
+		
+		//View = glm::rotate(View, rotation, glm::vec3(1, 1, 1));
+
+		// Model matrix : an identity matrix (model will be at the origin)
+		Model = glm::mat4(1.0f);  // Changes for each model !
+		// Our ModelViewProjection : multiplication of our 3 matrices
+		MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+		// Get a handle for our "MVP" uniform.
+		// Only at initialisation time.
+		
+ 
+		// Send our transformation to the currently bound shader,
+		// in the "MVP" uniform
+		// For each model you render, since the MVP will be different (at least the M part)
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		
+		
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//glDrawArrays(GL_TRIANGLES, 0, sphereCoordinates.size());
+		//glUniformMatrix4fv(TranslationID,1 , GL_FALSE, &Translate[0][0]);
+		for(int i = 0; i< 4; i++){
+			
+			move(timeChange, i);
+
+			glBindVertexArray(vao[i]);
+			glBindBuffer(GL_ARRAY_BUFFER, vbo[i]);
+			glUniformMatrix4fv(TranslationID, 1, GL_FALSE, &Translate[i][0][0]);
+
+			glDrawArrays(GL_TRIANGLES, 0,coordinates[i].size());
+
+		}
+	//	glDrawArrays(GL_LINES, 0, sphereNormals.size());
+
+	}
+
+};
 int main(void){
 	int running = GL_TRUE;
 	char currentKey = 'A';
 	SphereRotate sphereA;
 	coneRotate coneB;
 	SphereNormals normalsC;
+	Paths pathsE;
 	//initialise GLFW
 
 	if(!glfwInit()) {
@@ -531,7 +784,7 @@ int main(void){
 
 	glewInit();
 	
-	if(!(sphereA.init() && coneB.init() && normalsC.init()))
+	if(!(sphereA.init() && coneB.init() && normalsC.init() && pathsE.init()))
 		exit(EXIT_FAILURE);
 
 	while( running ) {
@@ -545,13 +798,15 @@ int main(void){
 			coneB.render();
 		} else if(currentKey == 'C'){
 			normalsC.render();
+		} else if(currentKey == 'E'){
+			pathsE.render();
 		}
 		//check("draw");
 		glfwSwapBuffers();
 		//check("swap buffers");
 		//check esc key
 
-		running= !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
+		running= !(glfwGetKey(GLFW_KEY_ESC) || glfwGetKey('Q')) && glfwGetWindowParam(GLFW_OPENED);
 		
 		if(glfwGetKey('A'))
 			currentKey = 'A';
@@ -559,6 +814,8 @@ int main(void){
 			currentKey='B';
 		else if(glfwGetKey('C'))
 			currentKey='C';
+		else if(glfwGetKey('E'))
+			currentKey = 'E';
 		glFlush();
 				
 	}
